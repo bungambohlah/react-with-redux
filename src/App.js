f37be378f21da40fd0caaff9d636b67f42efb4c7
afif <afif@garasilabs.com>
2021-01-04 17:40:41 +0700

    add react router with lazy loading

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import { User } from "./features/users/User";
 import "./App.css";
 
+// const { Counter } = lazy(() => import("./features/counter/Counter"));
+// const { User } = lazy(() => import("./features/users/User"));
+const Counter = lazy(() =>
+  import("./features/counter/Counter").then((module) => ({
+    default: module.Counter,
+  }))
+);
+const User = lazy(() =>
+  import("./features/users/User").then((module) => ({
+    default: module.User,
+  }))
+);
+const Home = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <span>Hello World!</span>
+      <span>
+        Try go to <Link to="/counter">Counter</Link>
+      </span>
+      <span>
+        Try go to <Link to="/user">User</Link>
+      </span>
+    </header>
+  </div>
+);
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {<User />}
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <Counter /> */}
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/counter" component={Counter} />
+          <Route path="/user" component={User} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
--- src/features/users/User.js
+++ src/features/users/User.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectUser, setUser, delUser } from "../users/userSlice";
+import styles from "./User.module.css";
 
 export function User() {
   const users = useSelector(selectUser);
@@ -10,28 +11,32 @@ export function User() {
 
   return (
     <div>
-      list of users:
-      <pre>
-        {users.map((v) => (
-          <div onClick={(e) => dispatch(delUser(v))}>{v}</div>
-        ))}
-      </pre>
-      <br />
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          setCurrUser("");
-          inputEl.current.focus();
-          dispatch(setUser(currUser));
-        }}
-      >
-        <input
-          type="text"
-          ref={inputEl}
-          onChange={(e) => setCurrUser(e.target.value)}
-          value={currUser}
-        ></input>
-      </form>
+      <div className={styles.row}>list of users:</div>
+      <div className={styles.row}>
+        <pre>
+          {users.map((v) => (
+            <div onClick={(e) => dispatch(delUser(v))}>{v}</div>
+          ))}
+        </pre>
+        <br />
+      </div>
+      <div className={styles.row}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setCurrUser("");
+            inputEl.current.focus();
+            dispatch(setUser(currUser));
+          }}
+        >
+          <input
+            type="text"
+            ref={inputEl}
+            onChange={(e) => setCurrUser(e.target.value)}
+            value={currUser}
+          ></input>
+        </form>
+      </div>
     </div>
   );
 }
